a67cd9ad976a567ebbf8145f795b9731dd339269
Molly Pace <mollypace@Mollys-MacBook-Air.local>
2019-03-04 16:50:42 +0000

    adding in some lines for plotting, and doing some debugging

--- h-h.py
+++ h-h.py
@@ -15,23 +15,21 @@ g_na = 120  # same as above for sodium
 g_r = 0.3  # same as above for r
 
 # set voltages for m, n and h
-e_n = (-12)
-e_m = 115
-e_h = 10.613
+e = [-12,115,10.613]
 
 
 # external input - to be input by user
 
 # i_ext = input("what is the external input to the neuron ?")
 
-i_ext = 10  # set myself for now as input seems to still think it's type string
+i_ext = 60  # set myself for now as input seems to still think it's type string
 print("You have set the external input to", i_ext)
 type(i_ext)
 
 # resting potential of neuron - input by user
 # rest_pot = input("what is the neuron resting potential? Please make this a zero or negative integer")
-rest_pot = -10  # set myself for now as input seems to still think it's type string
-print("you have set the membrane resting potential to", rest_pot, "mv")
+mem_pot = -65  # set myself for now as input seems to still think it's type string
+print("you have set the membrane resting potential to", mem_pot, "mv")
 
 # initial time set to zero
 current_time = 0
@@ -40,7 +38,7 @@ current_time = 0
 x = [0, 0, 1]
 
 # time step for the eular integration method
-t_step = 0.01
+t_step = 0.1
 
 # Eular integration method
 
@@ -49,64 +47,72 @@ t_step = 0.01
 # def integration(t_step, i_ext, rest_pot, e_n, e_m, w_h):
 # taken out function definition for the moment - put back in or rearrange to make use of classes
 
-t = sc.arange(-30, 10, 0.01)
+t = sc.arange(0, 50, t_step)
 alpha = [None]*3 # set empty list of alphas to change
 beta = [None]*3  # set empty list of beta to change
 x_0 = [None]*3  # empty list for x_0
 gnmh = [None]*3  # empty list for gnmh
 tau = [None]*3
-current_pot = rest_pot
-for i in t[:]:
-#for i in t:
-    #if t(i) == 10:
-     #       i_ext = 5  # starts the external current at time 10
-    #elif t(i) == 40:  #stops the external current at time 40
-     #   i_ext = 0
+mem_pot = mem_pot
+potential = np.array(mem_pot)
+time = np.array(0)
+for i in t:
+    if t.any() == 10:
+        i_ext = 5  # starts the external current at time 10
+    elif t.any() == 40:  # stops the external current at time 40
+        i_ext = 0
+    else:
 
-        # h-h alpha functions- assign values to empty list above
-    alpha[0] = (10 - current_pot)/(100*(np.exp((10 - current_pot)/10)-1))
-    alpha[1] = (25 - current_pot)/ (10*(np.exp((25 - current_pot)/10)-1))
-    alpha[2] = 0.07 * np.exp((-rest_pot)/20)
+    # h-h alpha functions- assign values to empty list above
+        alpha[0] = (10 - mem_pot)/(100 * (np.exp((10 - mem_pot)/10)-1))
+        alpha[1] = (25 - mem_pot)/(10 * (np.exp((25 - mem_pot)/10)-1))
+        alpha[2] = 0.07 * (np.exp((-mem_pot)/20))
+        #print(alpha)
 
-        # h-h beta functions -assign values to empty list above
-    beta[0] = 0.125 * np.exp((-current_pot)/80)
-    beta[1] = 4 * (np.exp((- current_pot)/18))
-    beta[2] = 1/(np.exp(((30 - current_pot)/10)+1))
+    # h-h beta functions -assign values to empty list above
+        beta[0] = 0.125*(np.exp((-mem_pot)/80))
+        beta[1] = 4*np.exp((-mem_pot)/18)
+        beta[2] = 1/(np.exp((30-mem_pot)/10)+1)
 
-    # tau and x_0 redefined with regards to alpha and beta
-    a_b = [x + y for x, y in zip(alpha, beta)] # element-wise summation of alpha and beta
-    #print(a_b) #used for some debugging
-    tau = [1/x for x in a_b]
-    x_0 = [x * y for x, y in zip(alpha, tau)]
+        # tau and x_0 redefined with regards to alpha and beta
+        a_b = [x + y for x, y in zip(alpha, beta)]  # element-wise summation of alpha and beta
+        # print(a_b) #used for some debugging
+        tau = [1/x for x in a_b]
+        #print(tau)
+        x_0 = [x * y for x, y in zip(alpha, tau)]
 
-    # leaky integration part
-    #x[i] = ((1 - t_step)/tau[i]) * (x + (t_step/(tau[i] * x_0[q])))
+        # leaky integration part
+        #x[i] = ((1 - t_step)/tau[i]) * (x + (t_step/(tau[i] * x_0[q])))
 
-        # conductance calculations
-    gnmh[0] = g_k * (x[0]**4)
-    gnmh[1] = g_na * ((x[1]**3) * x[2])
-    gnmh[2] = g_r
+            # conductance calculations
+        gnmh[0] = g_k * (x[0]**4)
+        gnmh[1] = g_na * ((x[1]**3) * x[2])
+        gnmh[2] = g_r
 
-        # ohms law for current
+            # ohms law for current
+        minus = [mem_pot - x for x in e ]
+        current = [x * y for x, y in zip(gnmh, minus)]
 
-    current_n = gnmh[0] * (rest_pot - e_n)
-    current_m = gnmh[1] * (rest_pot - e_m)
-    current_h = gnmh[2] * (rest_pot - e_h)
-    current_list = [current_n, current_m, current_h]
-    current = sum(current_list)
+            # update membrane potential
 
-        # update membrane potential
-    current_pot = current_pot + (t_step * (i_ext - current))
-
-        # storage of values needed for plotting graph
-    for i in (t>0):
-        current_time = current_time + 1
-        x_plot = np.array(t)
-        y_plot = np.array(current_pot)
+        mem_pot = mem_pot + (t_step * (i_ext - sum(current)))
+        potential = np.append(potential, mem_pot)
 
 
+            # storage of values needed for plotting graph
+        for i in (t>0):
+            current_time = current_time + 1
+            #time = np.array(t)
+            time = np.append(time, t)
+        #potential = np.array(potential)
 
-plt.plot(x_plot, y_plot)
 
+fig, ax = plt.subplots()
+ax.plot(time, potential)
+ax.set(xlabel = "time", ylabel = "membrane potential (mV")
+ax.grid
+fig.savefig("csc8311.png")
+plt.show()
 
+print("finished")
 
